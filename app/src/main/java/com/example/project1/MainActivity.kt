bef58302473ff9e4bcdbbc577c757ebec1fa9f6f
huaihsunalin <s9500211@gmail.com>
2022-04-08 16:31:12 -0700

    commit

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -8,4 +8,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    val foodtruck = listOf(
+        FoodTruck(
+            id: 0,
+            name: "Authentic Street Taco",
+            location: "Silo Patio",
+            open_time: "11am - 4pm",
+            description: "A unique blend of street taco with high-quality ingredients to give you the highest quality Mexican food.",
+            website_link: "https://www.authenticstreettaco.com/"
+        ),
+    )
+
+
+
 }
