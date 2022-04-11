a21eca6904724c8f9329ea6c4061073dc1b464ce
shparekh00 <shivaniparekh0730@gmail.com>
2022-04-11 11:31:56 -0700

    on phase 3

--- app/src/main/java/com/example/project1/FoodTruckDetail.kt
+++ app/src/main/java/com/example/project1/FoodTruckDetail.kt
@@ -1,4 +1,16 @@
 package com.example.project1
 
+import androidx.appcompat.app.AppCompatActivity
+
 class FoodTruckDetail {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_truck_detail)
+
+//        val intent = Intent(Intent.ACTION_VIEW, uri)
+//        startActivity(intent);
+    }
+
+
 }
