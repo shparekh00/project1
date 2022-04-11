58045f5376fabc525fe694e825693971012d978b
shparekh00 <shivaniparekh0730@gmail.com>
2022-04-11 16:32:08 -0700

    started filling out RecyclerViewAdapter

--- app/src/main/java/com/example/project1/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/project1/RecyclerViewAdapter.kt
@@ -1,5 +1,35 @@
 package com.example.project1
 
-class RecyclerViewAdapter (private var foodtrucks: List<FoodTruck>) {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
+class RecyclerViewAdapter(private var foodtrucks: List<FoodTruck>)
+    : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val titleTextView : TextView = itemView.findViewById(R.id.name)
+        val locationTextView : TextView = itemView.findViewById(R.id.location)
+        val timeTextView : TextView = itemView.findViewById(R.id.open_time)
+        val foodImageView : ImageButton = itemView.findViewById(R.id.foodImageButton)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.food_truck_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
